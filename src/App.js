0fcbf9b70c43c9a299b5e2843077e712ff9c54b4
Danil <bloodshoww@mail.ru>
2023-11-28 13:38:43 +0300

    fix: the code structure has been changed, and the class names have also been changed

--- src/App.js
+++ src/App.js
@@ -21,10 +21,8 @@ const dataProgram = [
 
 function App() {
   return (
-    <section className="information">
-      <div className="container">
-        <div className="wrapper">
-          <div className="conditions">
+    <section className="conditions container">
+          <div className="conditions__inner">
             <h2 className="conditions__title">Умови отриманння доходу</h2>
             <p className="conditions__descr">
               Основний план Amway – модель, яка з 1959 року допомагає людям у різних країнах отримувати дохід та покращувати якість життя.
@@ -34,26 +32,24 @@ function App() {
               <Button>Для ПУ</Button>
             </div>
           </div>
-          <div className="programs__blocks">
-            <div className="programs__block-1">
+          <div className="conditions__blocks">
+            <div className="conditions__first-block">
               {dataProgram.map((item) => (
-                  <div key={item.id} className="programs__item-1">
-                    <img className="programs__image" src={item.img} alt="Program" />
-                    <h3 className="programs__item-title">{item.title}</h3>
-                    <p className="programs__item-descr">{item.descr}</p>
+                  <div key={item.id} className="conditions__item-1">
+                    <img className="conditions__image" src={item.img} alt="Program" />
+                    <h3 className="conditions__item-title">{item.title}</h3>
+                    <p className="conditions__item-descr">{item.descr}</p>
                   </div>
               ))}
             </div>
-            <div className="programs__block-2">
-              <div className="programs__item-2">
-                <img className="programs__image" src={programThree} alt="Program "/>
-                <h3 className="programs__item-title">Програма дискреційних заохочень Бізнес Плюс⁺</h3>
-                <p className="programs__item-descr">Додаткові винагороди: виконуйте умови програми, розвивайте стабільний бізнес та відкривайте нові переваги співпраці з Amway!</p>
+            <div className="conditions__second-block">
+              <div className="conditions__item-2">
+                <img className="conditions__image" src={programThree} alt="Program "/>
+                <h3 className="conditions__item-title">Програма дискреційних заохочень Бізнес Плюс⁺</h3>
+                <p className="conditions__item-descr">Додаткові винагороди: виконуйте умови програми, розвивайте стабільний бізнес та відкривайте нові переваги співпраці з Amway!</p>
               </div>
             </div>
           </div>
-        </div>
-      </div>
     </section>
   );
 }
