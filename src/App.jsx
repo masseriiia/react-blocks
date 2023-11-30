ec83afd2d33156f68716a68cfd9f7b134f298047
Danil <bloodshoww@mail.ru>
2023-11-30 11:44:53 +0300

    feat: add block conditions

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Solution from './components/Solution/index.jsx';
 import './scss/app.scss'
+import Conditions from "./components/Conditions/index.jsx";
 
 function App() {
   return (
     <div className="container">
       <Solution />
+      <Conditions/>
     </div>
   );
 }
