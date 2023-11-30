19b9b0df14a6ed17a88105d80f67ec60f0753947
Danil <bloodshoww@mail.ru>
2023-11-30 13:54:36 +0300

    feat: add PropTypes to Button and BlockItem components and plugins

--- src/common/Button/index.jsx
+++ src/common/Button/index.jsx
@@ -1,11 +1,12 @@
-import style from './Button.module.scss'
+import PropTypes from 'prop-types';
+import style from './Button.module.scss';
 
-const Button = ({children}) => {
-    return (
-        <button className={style.button}>
-            {children}
-        </button>
-    );
+const Button = ({ children }) => {
+  return <button className={style.button}>{children}</button>;
+};
+
+Button.propTypes = {
+  children: PropTypes.node.isRequired,
 };
 
 export default Button;
--- src/components/Blocks/BlockItem/index.jsx
+++ src/components/Blocks/BlockItem/index.jsx
@@ -1,3 +1,5 @@
+import PropTypes from 'prop-types';
+
 const BlockItem = ({ name, descr }) => {
   return (
     <div className="solution__block">
@@ -5,6 +7,11 @@ const BlockItem = ({ name, descr }) => {
       <p className="solution__block-descr">{descr}</p>
     </div>
   );
+};
+
+BlockItem.propTypes = {
+  name: PropTypes.string.isRequired,
+  descr: PropTypes.string.isRequired,
 };
 
 export default BlockItem;
